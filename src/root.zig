7fcc868b6bdfacf33b19a2f039b4ad80a2c5776b
Kirk <kirkmacmillan88@gmail.com>
2025-12-07 05:15:23 -0600

    Refactor toThreadSafe function for improved allocator type handling

--- src/root.zig
+++ src/root.zig
@@ -18,18 +18,36 @@ pub const ByteSize = utils.ByteSize;
 pub const MemoryRegion = utils.MemoryRegion;
 
 pub fn toThreadSafe(allocator: anytype) ThreadSafeAllocator {
-    switch (@TypeOf(allocator)) {
-        std.mem.Allocator => return ThreadSafeAllocator.init(allocator),
-        ThreadSafeAllocator => return allocator,
-        StackAllocator => return ThreadSafeAllocator.init(allocator.allocator()),
-        DebugAllocator => return ThreadSafeAllocator.init(allocator.allocator()),
-        CountingAllocator => return ThreadSafeAllocator.init(allocator.allocator()),
-        PoolAllocator => return ThreadSafeAllocator.init(allocator.allocator()),
-        ScopedAllocator => return ThreadSafeAllocator.init(allocator.allocator()),
-        else => @compileError("Unsupported allocator type"),
+    const allocator_type = @TypeOf(allocator);
+    if (allocator_type == std.mem.Allocator) {
+        return ThreadSafeAllocator.init(allocator);
+    } else if (@typeInfo(allocator_type) == .@"struct" and @hasDecl(allocator_type, "allocator")) {
+        const decl_type_info = @typeInfo(@TypeOf(@field(allocator_type, "allocator")));
+        if (decl_type_info == .@"fn" and decl_type_info.@"fn".return_type == std.mem.Allocator) {
+            return ThreadSafeAllocator.init(@constCast(&allocator).allocator());
+        }
     }
+    @compileError("Unsupported allocator type: " ++ @typeName(allocator_type));
 }
 
 test {
     std.testing.refAllDeclsRecursive(@This());
+}
+
+test "toThreadSafe works with various allocator types" {
+    const sa = StackAllocator.init(1024);
+    const tsa1 = toThreadSafe(sa);
+    _ = tsa1;
+
+    const da = DebugAllocator.init(std.heap.page_allocator);
+    const tsa2 = toThreadSafe(da);
+    _ = tsa2;
+
+    const ca = CountingAllocator.init(std.heap.page_allocator);
+    const tsa3 = toThreadSafe(ca);
+    _ = tsa3;
+
+    const tsa4 = ThreadSafeAllocator.init(std.heap.page_allocator);
+    const tsa5 = toThreadSafe(tsa4);
+    _ = tsa5;
 }
