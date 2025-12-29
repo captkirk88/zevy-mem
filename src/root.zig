a015c8ba395ccfb10dd6a107b6bf830050668bc5
Kirk <kirkmacmillan88@gmail.com>
2025-12-29 17:17:41 -0600

    Update dependencies and add initArcWithMutex function for thread-safe memory management

--- src/root.zig
+++ src/root.zig
@@ -20,15 +20,46 @@ pub const Mutex = @import("mutex.zig").Mutex;
 pub const Rc = @import("pointers/rc.zig").Rc;
 pub const Arc = @import("pointers/arc.zig").Arc;
 
+pub fn initArcWithMutex(allocator: std.mem.Allocator, value: anytype) !*Arc(*Mutex(@TypeOf(value))) {
+    const ValueType = @TypeOf(value);
+    const mutex_ptr = try Mutex(ValueType).init(allocator, value);
+    return Arc(*Mutex(ValueType)).init(allocator, mutex_ptr);
+}
+
 pub fn toThreadSafe(allocator: anytype) std.heap.ThreadSafeAllocator {
     const allocator_type = @TypeOf(allocator);
     if (allocator_type == std.mem.Allocator) {
         return std.heap.ThreadSafeAllocator{ .child_allocator = allocator };
-    } else if (@typeInfo(allocator_type) == .@"struct" and @hasDecl(allocator_type, "allocator")) {
-        const decl_type_info = @typeInfo(@TypeOf(@field(allocator_type, "allocator")));
-        if (decl_type_info == .@"fn" and decl_type_info.@"fn".return_type == std.mem.Allocator) {
-            return std.heap.ThreadSafeAllocator{ .child_allocator = @constCast(&allocator).allocator() };
-        }
+    } else if (allocator_type == *StackAllocator or allocator_type == *const StackAllocator) {
+        const sa: *StackAllocator = @ptrCast(@constCast(allocator));
+        return std.heap.ThreadSafeAllocator{
+            .child_allocator = sa.allocator(),
+        };
+    } else if (allocator_type == *DebugAllocator or allocator_type == *const DebugAllocator) {
+        const da: *DebugAllocator = @ptrCast(@constCast(allocator));
+        return std.heap.ThreadSafeAllocator{
+            .child_allocator = da.allocator(),
+        };
+    } else if (allocator_type == *CountingAllocator or allocator_type == *const CountingAllocator) {
+        const ca: *CountingAllocator = @ptrCast(@constCast(allocator));
+        return std.heap.ThreadSafeAllocator{
+            .child_allocator = ca.allocator(),
+        };
+    } else if (allocator_type == *CautiousAllocator or allocator_type == *const CautiousAllocator) {
+        const ca: *CautiousAllocator = @ptrCast(@constCast(allocator));
+        return std.heap.ThreadSafeAllocator{
+            .child_allocator = ca.allocator(),
+        };
+    } else if (allocator_type == *SafeAllocator or allocator_type == *const SafeAllocator) {
+        const sa: *SafeAllocator = @ptrCast(@constCast(allocator));
+        return std.heap.ThreadSafeAllocator{
+            .child_allocator = sa.allocator(),
+        };
+    } else if (allocator_type == *GuardedAllocator or allocator_type == *const GuardedAllocator) {
+        const ga: *GuardedAllocator = @ptrCast(@constCast(allocator));
+        return std.heap.ThreadSafeAllocator{
+            .child_allocator = ga.allocator(),
+        };
     }
     @compileError("Unsupported allocator type: " ++ @typeName(allocator_type));
 }
@@ -39,14 +70,75 @@ test {
 
 test "toThreadSafe works with various allocator types" {
     const sa = StackAllocator.init(1024);
-    const tsa1 = toThreadSafe(sa);
+    const tsa1 = toThreadSafe(&sa);
     _ = tsa1;
 
     const da = DebugAllocator.init(std.heap.page_allocator);
-    const tsa2 = toThreadSafe(da);
+    const tsa2 = toThreadSafe(&da);
     _ = tsa2;
 
     const ca = CountingAllocator.init(std.heap.page_allocator);
-    const tsa3 = toThreadSafe(ca);
+    const tsa3 = toThreadSafe(&ca);
     _ = tsa3;
+}
+
+test "initArcWithMutex - multithreaded mutation" {
+    const testing = std.testing;
+    const allocator = testing.allocator;
+
+    const init_value: i32 = 0;
+    const arc = try initArcWithMutex(allocator, init_value);
+    defer arc.deinit();
+
+    const ThreadContext = struct {
+        arc_ptr: *Arc(*Mutex(i32)),
+        iterations: usize,
+    };
+
+    const worker = struct {
+        fn run(ctx: ThreadContext) void {
+            const local_arc = ctx.arc_ptr.clone();
+            defer local_arc.deinit();
+
+            var i: usize = 0;
+            while (i < ctx.iterations) : (i += 1) {
+                var guard = local_arc.get().*.lock();
+                guard.get().* += 1;
+                // release the lock early so we can sleep outside the critical section
+                guard.deinit();
+
+                // occasionally sleep to increase thread interleavings and help expose race conditions
+                if (i % 17 == 0) {
+                    // sleep for ~1ms
+                    std.Thread.sleep(1 * std.time.ns_per_ms);
+                }
+            }
+        }
+    }.run;
+
+    const thread_count = 8;
+    const iterations = 1000;
+    var threads: [thread_count]std.Thread = undefined;
+
+    var i: usize = 0;
+    while (i < thread_count) : (i += 1) {
+        threads[i] = try std.Thread.spawn(.{}, worker, .{ThreadContext{
+            .arc_ptr = arc,
+            .iterations = iterations,
+        }});
+    }
+
+    for (threads) |thread| {
+        thread.join();
+    }
+
+    // Verify the final value
+    {
+        const guard = arc.get().*.lock();
+        defer guard.deinit();
+        const final_count = guard.get().*;
+        try testing.expectEqual(thread_count * iterations, final_count);
+    }
+
+    try testing.expectEqual(1, arc.strongCount());
 }
